c85d8c2ddb5d37c4c6618b48d4dede075f7c1d8c
이단영 <danyeong@idan-yeong-ui-MacBookAir.local>
2024-05-06 11:22:17 +0900

    fix torch version

--- setting.sh
+++ setting.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install torch==2.0.0 torchvision==0.15.1 torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cu118
 pip install rdkit wandb hydra-core hydra-colorlog torch_ema e3nn
 pip install torch_geometric==1.7.2
 pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.0.0+cu118.html
