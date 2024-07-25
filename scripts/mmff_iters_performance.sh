bde5261847046874d452b10e063f0dec2dd5e504
Danyeong Lee <81910252+DanyeongLee@users.noreply.github.com>
2024-07-25 10:16:22 +0900

    Delete scripts directory

--- scripts/mmff_iters_performance.sh
+++
@@ -1,5 +0,0 @@
-for iters in 5 10 20 50 100 150 200 500 1000
-do  
-    python test.py +experiment=230709-fromtorsion-drugs-7steps-final gpu=0 train.test_mmff=true train.test_mmff_iters=${iters} train.test_starting_point=true
-    python test.py +experiment=230709-fromtorsion-drugs-7steps-final gpu=0 train.test_mmff=true train.test_mmff_iters=${iters}
-done
