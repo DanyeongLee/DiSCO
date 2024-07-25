01e88e559266a6b72060c572a51fe909c541c975
Danyeong Lee <81910252+DanyeongLee@users.noreply.github.com>
2024-07-25 10:29:04 +0900

    Create common.py

---
+++ disco/utils/common.py
@@ -0,0 +1,13 @@
+import os
+import random
+import numpy as np
+import torch
+
+def seed_everything(seed: int = 42):
+    random.seed(seed)
+    np.random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)  
+    torch.backends.cudnn.deterministic = True  
+    torch.backends.cudnn.benchmark = True  
